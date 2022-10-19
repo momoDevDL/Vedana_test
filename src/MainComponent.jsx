37453644c183d87f5be3fa711ab4083e803cf6e5
momoDevDL <mohamedmasbahaboulaich@gmail.com>
2022-10-19 16:05:40 +0200

    FE - Initial commit -

---
+++ src/MainComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainComponent = () => {
+    return (
+        <div>
+            <h1> Hello Vedana ! </h1>
+        </div>
+    )
+}
+
+export default MainComponent;
